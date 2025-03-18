a1b4779b64ebeb2c7c7140fc0885fbae2f55446e
Latta <info@latta.ai>
2025-03-18 17:09:45 +0100

    Add division by zero error handling

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ latta = Latta('6057d68e93993a5a91b44ba43972aba36fb35400b51c91d7e67c3cad4ee11b18a
 
 @latta.wrap
 def divide(x, y):
-    return x / y
+    try:
+        return x / y
+    except ZeroDivisionError:
+        return None
 
 if __name__ == "__main__":
     print(divide(10, 5))
