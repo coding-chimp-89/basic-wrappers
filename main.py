2a9c2e99e517601f4f6f44571325b6b44c6ea7ca
Coding Chimp <vkcoding89@gmail.com>
2025-03-18 16:46:32 +0100

    latte recorder

---
+++ main.py
@@ -0,0 +1,7 @@
+from latta import Latta
+
+latta = Latta('6057d68e93993a5a91b44ba43972aba36fb35400b51c91d7e67c3cad4ee11b18ac6533b967a41a3c2c56b771131b41bfcd924d62aa9f7cda24f5a8456c12c2b9')
+
+@latta.wrap
+def divide(x, y):
+    return x / y
